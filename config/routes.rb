4fbc1fb3b3d7f5823f3164cbc6f1887ef3d9fded
nnhai-naitei-intern <nguyen.nhat.hai@sun-asterisk.com>
2020-07-29 13:38:01 +0700

    Chapter3: Static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /en|vi/ do
+    root "static_pages#home"
+    
+    get "static_pages/home"
+    get "static_pages/help"
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
