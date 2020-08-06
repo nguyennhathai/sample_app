7fb82b91c9b387d1e3c608e50640be17b0c86edb
nnhai-naitei-intern <nguyen.nhat.hai@sun-asterisk.com>
2020-08-06 16:57:04 +0700

    Chapter10: Updating, showing, anddeleting users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_05_075630) do
+ActiveRecord::Schema.define(version: 2020_08_06_064809) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 2020_08_05_075630) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
     t.string "remember_digest"
+    t.boolean "admin", default: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
